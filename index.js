5fbf3a169a1df0f9ff92bcf067b3e71154b33b69
moureen mutinda <maurinemutinda72@gmail.com>
2024-12-19 11:32:48 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+// Variables for multiplication
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; // Result: 62
+
+// Variable for a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1; // Random integer > 0
+
+// Variables for modulus operation
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; // Result: 4
+
+// Variable to find the maximum number in a set
+const max = Math.max(5, 10, 15, 20); // Result: 20
